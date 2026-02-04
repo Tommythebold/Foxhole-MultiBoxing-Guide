afde1248e7963d235368a96e3ec712a5ca12bbc4
Tommythebold <tommythebold16@gmail.com>
2026-02-04 01:00:34 -0700

    Update _Launch All Autoclickers.bat

--- Sandboxie/_Launch All Autoclickers.bat
+++ Sandboxie/_Launch All Autoclickers.bat
@@ -1,6 +1,6 @@
 @echo off
-REM Change the path below to your actual AutoHotkey.exe and script location
 START "" "C:\Foxhole Multiboxing\AutoClickers\Foxhole - OMB 1.ahk"
 echo AutoHotkey script started.
 timeout /t 0 /nobreak >nul
 exit
+
